d54135f91813e8684c1e7c36bf7dfe5e6a895685
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2021-05-29 18:50:06 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor() {
+document.getElementById("Heading1").style.color = "green"; 
+}
+   function setdate(){
+   document.getElementById("date").innerHTML=new Date().toDateString();    
+   }
